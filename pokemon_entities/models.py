a2d82a419248119b41e8bc49112a4a2b1d6370fc
Izdator <evarborisov@gmail.com>
2025-01-29 15:26:02 +0300

    Some fields in the models are made optional.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -6,7 +6,7 @@ class Pokemon(models.Model):
     title_en = models.CharField(max_length=200, verbose_name='Название на английском')
     title_jp = models.CharField(max_length=200, verbose_name='Название на японском')
     image = models.ImageField(upload_to='pokemon_images/', blank=True, null=True, verbose_name='Изображение')
-    description = models.CharField(max_length=500, verbose_name='Описание')
+    description = models.CharField(max_length=500, blank=True, verbose_name='Описание')
 
     evolution = models.ForeignKey(
         'self',
@@ -32,8 +32,8 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, related_name='entities', on_delete=models.CASCADE, verbose_name='Покемон')
-    lat = models.FloatField(verbose_name='Широта')
-    lon = models.FloatField(verbose_name='Долгота')
+    lat = models.FloatField(verbose_name='Широта', blank=True)
+    lon = models.FloatField(verbose_name='Долгота', blank=True)
     appeared_at = models.DateTimeField(blank=True, null=True, verbose_name='Появился в')
     disappeared_at = models.DateTimeField(blank=True, null=True, verbose_name='Исчез в')
     level = models.IntegerField(default=1, verbose_name='Уровень')
@@ -44,4 +44,3 @@ class PokemonEntity(models.Model):
 
     def __str__(self):
         return f'{self.pokemon.title_ru} (Lat: {self.lat}, Lon: {self.lon}, Level: {self.level})'
-    
