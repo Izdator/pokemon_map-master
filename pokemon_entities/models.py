358538fefddf40bd9ab13fe1d958d4ea7c68917e
Izdator <evarborisov@gmail.com>
2025-01-30 17:56:15 +0300

    Some fields can be left empty.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,10 +3,12 @@ from django.db import models
 
 class Pokemon(models.Model):
     title_ru = models.CharField(max_length=200, verbose_name='Название на русском')
-    title_en = models.CharField(max_length=200, verbose_name='Название на английском')
-    title_jp = models.CharField(max_length=200, verbose_name='Название на японском')
+    title_en = models.CharField(max_length=200, blank=True,
+                                verbose_name='Название на английском')
+    title_jp = models.CharField(max_length=200, blank=True,
+                                verbose_name='Название на японском')
     image = models.ImageField(upload_to='pokemon_images/', blank=True, null=True, verbose_name='Изображение')
-    description = models.CharField(max_length=500, blank=True, verbose_name='Описание')
+    description = models.TextField(blank=True, verbose_name='Описание')
 
     def __str__(self):
         return f'{self.title_ru}'
@@ -20,10 +22,10 @@ class Evolution(models.Model):
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, related_name='entities', on_delete=models.CASCADE, verbose_name='Покемон')
-    lat = models.FloatField(verbose_name='Широта', blank=True)
-    lon = models.FloatField(verbose_name='Долгота', blank=True)
+    lat = models.FloatField(verbose_name='Широта', blank=True, null=True)
+    lon = models.FloatField(verbose_name='Долгота', blank=True, null=True)
     appeared_at = models.DateTimeField(blank=True, null=True, verbose_name='Появился в')
-    disappeared_at = models.DateTimeField(blank=True, null=True, verbose_name='Исчез в')
+    disappeared_at = models.DateTimeField(blank=True, null=True, verbose_name='Исчез в') 
     level = models.IntegerField(default=1, verbose_name='Уровень')
     health = models.IntegerField(default=100, verbose_name='Здоровье')
     strength = models.IntegerField(default=10, verbose_name='Сила')
